76af518e96f77753cf8a43db3e6b0e62af464caa
jjnietoc <juanjonietocastillo@gmail.com>
2022-12-10 15:59:22 -0500

    login

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 
 
 class NewUserForm(UserCreationForm):
@@ -15,4 +16,4 @@ class NewUserForm(UserCreationForm):
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
-        return user
+        return redirect('login')
