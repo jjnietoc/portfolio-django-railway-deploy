a0fb169956de7e0887189ff85a5311302e703e11
Juan José <105318570+jjnietoc@users.noreply.github.com>
2023-01-24 16:11:07 -0500

    "portafolio url login"

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/',include('users.urls')),
-    path('', include('portafolio.urls')),
+    path('',include('users.urls')),
+    path('portafolio/', include('portafolio.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
