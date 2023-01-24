29a83493e80c9c1ceb4dc0073c21084a11793e50
Juan José <105318570+jjnietoc@users.noreply.github.com>
2023-01-24 15:51:13 -0500

    "redirect"

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import portfolio, portafolio_import
 
 urlpatterns = [
-    path('portafolio/', portfolio, name='portafolio'),
+    path('', portfolio, name='portafolio'),
     path('portafolio_import', portafolio_import, name='portafolio_import')
 ] 
 
